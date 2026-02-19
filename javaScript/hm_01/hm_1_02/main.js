2bdbcb0e64db318d8ab6bcff65ddd0be692b776b
Megatron <vladyk.baidyk@gmail.com>
2026-02-19 17:06:07 +0200

    new js

---
+++ javaScript/hm_01/hm_1_02/main.js
@@ -0,0 +1,13 @@
+/*
+#6Qb97gsv
+
+– Створити 3 змінних firstName, middleName, lastName, 
+наповнити їх своїм ПІБ. З’єднати їх в одну змінну person 
+(Не об’єкт, просто за допомогою конкатенації)
+ */
+
+const firstName = "Vladyslav";
+const middleName = " Baidyk";
+const lastName = " Maksimovich";
+const person = firstName + middleName + lastName;
+console.log(person);
