a7733b86dfd3eea53f60498f24e7fac9fe954c4e
Megatron <vladyk.baidyk@gmail.com>
2026-03-06 15:16:49 +0200

    New js

---
+++ javaScript/hm_04/hm_04_06/main.js
@@ -0,0 +1,16 @@
+/*
+#hOL6126
+
+– створити функцію, яка створює ul з
+ трьома елементами li та виводить його через document.write. 
+ Текст li задати через аргумент всім однаковий
+ */
+function navigation(text){
+    document.write(`
+        <ul>
+        <li>${text}</li>
+        <li>${text}</li>
+        <li>${text}</li>
+        </ul>`)
+}
+console.log(navigation("text"))
